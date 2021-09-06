e216c55f98b5536beb77b3b6224265b386a0a6f3
Ladislaw <rodrigoladislausilva1996@gmail.com>
2021-09-06 16:21:14 -0300

    Commit inicial do projeto

---
+++ src/main/java/br/com/springboot/CidadeClienteApi/CidadeClienteApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.springboot.CidadeClienteApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CidadeClienteApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CidadeClienteApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/springboot/CidadeClienteApi/CidadeClienteApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.springboot.CidadeClienteApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CidadeClienteApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
